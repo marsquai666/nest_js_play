aed8334f8320462ebcb73b775c67a8a5785fecb5
marsquai <marsquai@gmail.com>
2022-06-23 02:38:25 +0900

    develop: herokuにdeployするまでの状態（ただしまだ動かない）

--- my-first-app/prisma/migrations/20220620190557_first/migration.sql
+++ my-first-app/prisma/migrations/20220620190557_first/migration.sql
@@ -1,21 +1,27 @@
 -- CreateTable
 CREATE TABLE "Post" (
-    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "id" SERIAL NOT NULL,
     "title" TEXT NOT NULL,
     "content" TEXT,
     "published" BOOLEAN NOT NULL DEFAULT false,
     "authorId" INTEGER,
-    CONSTRAINT "Post_authorId_fkey" FOREIGN KEY ("authorId") REFERENCES "Customer" ("id") ON DELETE SET NULL ON UPDATE CASCADE
+
+    CONSTRAINT "Post_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
 CREATE TABLE "Customer" (
-    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "id" SERIAL NOT NULL,
     "name" TEXT NOT NULL,
     "age" INTEGER NOT NULL,
     "email" TEXT NOT NULL,
-    "is_active" BOOLEAN NOT NULL
+    "is_active" BOOLEAN NOT NULL,
+
+    CONSTRAINT "Customer_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateIndex
 CREATE UNIQUE INDEX "Customer_email_key" ON "Customer"("email");
+
+-- AddForeignKey
+ALTER TABLE "Post" ADD CONSTRAINT "Post_authorId_fkey" FOREIGN KEY ("authorId") REFERENCES "Customer"("id") ON DELETE SET NULL ON UPDATE CASCADE;
